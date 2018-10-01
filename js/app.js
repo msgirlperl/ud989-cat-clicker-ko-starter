d0dbcd4061036bbc1c92c5cd83f2c83f13eaa4d0
msgirlperl <mspeert@gmail.com>
2018-10-01 16:23:54 -0400

    refactored to use 'with' binding context

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-const ViewModel = function() {
-    
+const Cat = function() {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
@@ -10,6 +9,13 @@ const ViewModel = function() {
         if (clicks < 20) return 'infant';
         return 'teen';
     }, this)
+   
+    this.cats = ko.observableArray([{name: 'Tabtab'}, {name: 'T-bone'}, {name: 'Mr. T'}, {name: 'Tabitha Tab Tabby Catty Cat'}]);
+};
+
+const ViewModel = function() {
+    this.currentCat = ko.observable( new Cat());
+
     this.incrementCounter = function() {
         this.clickCount(this.clickCount() + 1);
     };
